3766ed237a178f31584b66e2a2041959604d5880
KarKoun <radostjoyradost@gmail.com>
2024-10-18 21:27:51 +0300

    Folders have been created and plugins are sorted into them

--- init.lua
+++ init.lua
@@ -197,7 +197,10 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  { import = 'custom.plugins' },
+  { import = 'custom.plugins.visual' },
+  { import = 'custom.plugins.qol' },
+  { import = 'custom.plugins.new-features' },
+  { import = 'custom.plugins.pls' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
